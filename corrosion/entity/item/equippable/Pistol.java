a38ceb3152eb022f638fc486ade425f9d0fe9c45
Henry Lim <45436409+henrylimb69@users.noreply.github.com>
2018-12-12 14:56:46 -0500

    Update Pistol.java

--- corrosion/entity/item/equippable/Pistol.java
+++ corrosion/entity/item/equippable/Pistol.java
@@ -29,12 +29,63 @@ public class Pistol extends equippable{
   public Sprite sprite;
   
   
+    public static void init(){
+    try{
+      //loads icon
+      icon = ImageIO.read(new File("sprites/pistol/icon.png"));
+      //loads relaod animations
+      sprites[0] = new BufferedImage[4];
+      for (int i = 1; i <= 4; ++i){
+        sprites[0][i-1] = ImageIO.read(new File("sprites/pistol/animation/frame" + i + ".png"));
+      }
+
+      //loads shooting animations
+      sprites[1] = new BufferedImage[3];
+      for (int i = 0; i < 3; ++i){
+        sprites[1][i] = sprites[0][2-i];
+      }
+    }catch(Exception e){
+      //exits on error with message
+      System.out.println("Reading Pistol Sprite: " + e);
+      System.exit(-1);
+    }
+  }
+  
+  
   public Person(Player p){
     super(p);
     this.sprite = new Sprite(icon, new int[]{1,2}, sprites, new int[]{500,50});
   }
   
+    public void drawEquipped(Graphics g){
+    transform = player.getTransform();
+    transform.translate(-18, -110);
+    ((Graphics2D)(g)).drawImage(sprite.getFrame(), player.getTransform(), null);
+  }
   
+  public void draw(Graphics g, long t){}
+  
+  public void attack(Point p){
+    //checks if crossbow is reloaded
+    if (sprite.isState(SHOOT_READY, false)){
+      //creates a new arrow
+      new Arrow(player, p.getX(), p.getY());
+      //starts shoot animation
+      sprite.startAnimation(1);
+    }
+  }
+
+
+  
+  public void attack2(Point p){
+    reload();
+  }
+
+  public void reload(){
+    if (sprite.isState(RELOAD_READY, false)){
+      sprite.startAnimation(0);
+    }
+  }
 
   
 }
