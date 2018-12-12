a619c5d0ec7c3903888487a776c4bfbe43b4cc16
Henry Lim <45436409+henrylimb69@users.noreply.github.com>
2018-12-12 14:43:16 -0500

    Update Pistol.java

--- corrosion/entity/item/equippable/Pistol.java
+++ corrosion/entity/item/equippable/Pistol.java
@@ -1,1 +1,24 @@
+//Henry Lim
+//Dec 12, 2018
+//Pistol class
 package corrosion.entity.item.equippable;
+public class Pistol{
+  private 
+  
+  public Person(){
+    
+  }
+  
+  public Person(){
+    
+  }
+  
+
+
+public void drawEquipped(Graphics)
+public void attack(Point p)
+public void attack2(Point p)
+public void reload() 
+
+  
+}
