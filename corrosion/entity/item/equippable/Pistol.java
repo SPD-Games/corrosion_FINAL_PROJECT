b76c1288a39b52fa851c2e236e31c858c9df3621
Henry Lim <45436409+henrylimb69@users.noreply.github.com>
2018-12-12 14:52:14 -0500

    Update Pistol.java

--- corrosion/entity/item/equippable/Pistol.java
+++ corrosion/entity/item/equippable/Pistol.java
@@ -2,23 +2,39 @@
 //Dec 12, 2018
 //Pistol class
 package corrosion.entity.item.equippable;
-public class Pistol{
-  private 
-  
-  public Person(){
-    
-  }
-  
-  public Person(){
-    
-  }
-  
+
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.geom.AffineTransform;
+import java.awt.Point;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+import corrosion.Sprite;
+import corrosion.entity.Entity;
+import corrosion.entity.player.Player;
+import corrosion.entity.projectile.Arrow;
 
 
-public void drawEquipped(Graphics)
-public void attack(Point p)
-public void attack2(Point p)
-public void reload() 
+public class Pistol extends equippable{
+  private static BufferedImage icon;
+  private static BufferedImage[][] sprites = new BufferedImage[2][];
+  private final int[] SHOOT_READY = {0,3};
+  private final int[] RELOAD_READY = {1,2};
+  public Sprite sprite;
+  
+  
+  public Person(Player p){
+    super(p);
+    this.sprite = new Sprite(icon, new int[]{1,2}, sprites, new int[]{500,50});
+  }
+  
+  
 
   
 }
