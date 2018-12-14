22eb2d579d28243c79e553b46e684ad68a4cd1c3
Henry Lim <45436409+henrylimb69@users.noreply.github.com>
2018-12-14 15:21:18 -0500

    Update and rename sniper.java to Sniper.java

---
+++ corrosion/entity/item/equippable/Sniper.java
@@ -0,0 +1,25 @@
+//Henry Lim
+//Dec 14, 2018
+//Sniper class
+package corrosion.entity.item.equippable;
+
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.geom.AffineTransform;
+import java.awt.Point;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+import corrosion.Sprite;
+import corrosion.entity.Entity;
+import corrosion.entity.player.Player;
+import corrosion.entity.projectile.Arrow;
+
+public class Sniper extends Equippable{
+  
+}
