af423740b96562b62b08288f9c612022fa353f43
Eddie <eddiepei2360@gmail.com>
2018-12-14 18:47:49 -0500

    v0.0.4

--- corrosion/drawingstate/ConnectMenuDrawing.java
+++ corrosion/drawingstate/ConnectMenuDrawing.java
@@ -18,7 +18,6 @@ import java.awt.Graphics;
 import corrosion.Drawing;
 
 public class ConnectMenuDrawing extends DrawingState{
-  TextArea connectInput;
 
   /**
   * draw the connection Screen
@@ -26,22 +25,11 @@ public class ConnectMenuDrawing extends DrawingState{
   * @param t frame count
   */
   public void draw(Graphics g, long t) {
-    // stop the draw timer stoping the draw and allow the buttons to be drawn properly
-    (Drawing.getPanel()).getTimer().stop();
 
-    System.out.print(1);
-
-    g.setColor(new Color(255,69,0));
-    g.fillRect(0, 0, Drawing.width(), Drawing.height());
   }
 
   public void init(){
-    connectInput = new TextArea("Enter IP here");
-    connectInput.setPreferredSize(new Dimension(800,75));
-    connectInput.setFont(new Font("Serif", Font.PLAIN, 40));
-    connectInput.setRows(1);
-    connectInput.setBounds(Drawing.getPanel().getWidth()/2 - 100, Drawing.getPanel().getWidth()/2, 200, 75);
-    (Drawing.getPanel()).add(connectInput);
+
   }
 
 }
