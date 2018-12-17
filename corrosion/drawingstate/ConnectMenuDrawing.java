1f580c99610ae020082431ba21a12f4f5dde36db
Eddie <eddiepei2360@gmail.com>
2018-12-17 14:07:33 -0500

    v0.0.4

--- corrosion/drawingstate/ConnectMenuDrawing.java
+++ corrosion/drawingstate/ConnectMenuDrawing.java
@@ -14,11 +14,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.awt.geom.AffineTransform;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import corrosion.Drawing;
-import corrosion.drawingstate.menuobjects.*;
+import corrosion.drawingstate.menuobjects.ButtonG;
+import corrosion.input.*;
+import corrosion.input.bind.*;
 
 public class ConnectMenuDrawing extends DrawingState{
+  public ButtonG b = new ButtonG((Drawing.width())/2 - 50, (Drawing.height())/2 - 50,100 ,100 );
 
   /**
   * draw the connection Screen
@@ -26,12 +30,14 @@ public class ConnectMenuDrawing extends DrawingState{
   * @param t frame count
   */
   public void draw(Graphics g, long t) {
-    ButtonG b = new ButtonG();
+    ((Graphics2D)g).translate((Drawing.width())/2,(Drawing.height())/2 );
     b.draw(g);
   }
 
   public void init(){
-
+    MouseBindable mouseBinds[] = new MouseBindable[5];
+    mouseBinds[1] = new LeftClick();//left click
+    Mouse.setBinds(mouseBinds);
   }
 
 }
