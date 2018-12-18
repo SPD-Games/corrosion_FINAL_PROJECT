5ee0d187da007f57db31636887d99c8993a7c88a
root <root@localhost.localdomain>
2018-12-18 15:28:35 -0500

    made it so arrows have a max range

--- corrosion/entity/item/Arrow.java
+++ corrosion/entity/item/Arrow.java
@@ -23,7 +23,9 @@ import corrosion.Projectile;
 public class Arrow extends Item implements Projectile{
   private static BufferedImage img;
   private double xVel, yVel;
-  private double endXPos, endYPos;
+  private double range;
+  private static final double MAX_VEL = 2;
+  private static final double MAX_RANGE = 300;
   private double lastXPos, lastYPos;
   private Player player;
   /**
@@ -58,10 +60,9 @@ public class Arrow extends Item implements Projectile{
     this.yPos = player.getYPos() - 189 * Math.cos(rotation);
     this.lastXPos = xPos;
     this.lastYPos = yPos;
-    this.endXPos = player.getXPos() + mouseX;
-    this.endXPos = player.getYPos() + mouseY;
-    this.xVel = 1*Math.sin(rotation);
-    this.yVel = 1*-Math.cos(rotation);
+    range = MAX_RANGE;
+    this.xVel = MAX_VEL*Math.sin(rotation);
+    this.yVel = MAX_VEL*-Math.cos(rotation);
     this.transform = new AffineTransform();
 
     //add to active entities
@@ -83,20 +84,19 @@ public class Arrow extends Item implements Projectile{
   * @param t the time passes since the last frame
   */
   public void draw(Graphics g, long t){
-    //checks if the item is shot
-    if (player != null){
-      //updates the position
-      xPos += t * xVel;
-      yPos += t * yVel;
+    xPos += t * xVel;
+    yPos += t * yVel;
+    range -= MAX_VEL;
+    if (range < 0){
+      xVel = 0;
+      yVel = 0;
     }
-
-    //draws hit line
 
     //draws the arrow
     transform.setToTranslation(xPos - 3, yPos);
     transform.rotate(rotation, 3, 0);
     ((Graphics2D)(g)).drawImage(img, transform, null);
-    ((Graphics2D)(g)).drawLine((int)xPos, (int)yPos, (int)lastXPos, (int)lastYPos);
+    //((Graphics2D)(g)).drawLine((int)xPos, (int)yPos, (int)lastXPos, (int)lastYPos);
     lastXPos = xPos;
     lastYPos = yPos;
   }
