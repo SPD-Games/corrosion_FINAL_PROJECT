aeb2f57c6c55be02f2c687a2f91d4d463fbd324a
Eddie <eddiepei2360@gmail.com>
2018-12-31 15:58:09 -0500

    v0.0.1

--- corrosion/Main.java
+++ corrosion/Main.java
@@ -2,23 +2,22 @@
   * December 13 2018
   * Runs the game
   * TODO:
-      hit reg
-      finish projectile server
+      hit reg and max distance of projectiles
   */
 
 package corrosion;
 
 import corrosion.Window;
 
-public class Main{
-  public static void init(){
+public class Main {
+  public static void init() {
     System.setProperty("sun.java2d.opengl", "true");
   }
 
   /**
    * @param args command line arguments
   */
-  public static void main(String[] args){
+  public static void main(String[] args) {
     init();
 
     new Window();
---
+++ corrosion/entity/item/equippable/Apple.java
@@ -0,0 +1,85 @@
+//Edward Pei
+//Dec 29, 2018
+//Apple class
+package corrosion.entity.item.equippable;
+
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.geom.AffineTransform;
+import java.awt.Point;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+import corrosion.Sprite;
+import corrosion.entity.Entity;
+import corrosion.entity.player.Player;
+import corrosion.entity.projectile.Arrow;
+
+public class Apple extends Equippable{
+
+  private static BufferedImage icon;
+  private static BufferedImage[][] sprites = new BufferedImage[2][];
+  public Sprite sprite;
+
+  public static void init(){
+    try{
+      //loads icon
+      icon = ImageIO.read(new File("sprites/apple/icon.png"));
+      //loads eating animations
+      sprites[0] = new BufferedImage[4];
+      for (int i = 1; i <= 4; ++i){
+        sprites[0][i-1] = ImageIO.read(new File("sprites/apple/animation/frame" + i + ".png"));
+      }
+
+      //loads shooting animations
+      sprites[1] = new BufferedImage[3];
+      for (int i = 0; i < 3; ++i){
+        sprites[1][i] = sprites[0][2-i];
+      }
+    }catch(Exception e){
+      //exits on error with message
+      System.out.println("Reading apple Sprite: " + e);
+      System.exit(-1);
+    }
+  }
+
+  public Sniper(Player p){
+
+  }
+
+  public void drawEquipped(Graphics g){
+    transform = player.getTransform();
+    transform.translate(-18, -110);
+    ((Graphics2D)(g)).drawImage(sprite.getFrame(), player.getTransform(), null);
+  }
+
+  public void draw(Graphics g, long t){}
+
+  ///////
+
+  public void attack(Point p){
+
+  }
+
+  /**
+  * Reloads the weapon
+  * @param p the pointer position on the screen relative to the player
+  */
+  public void attack2(Point p){
+
+  }
+
+  /**
+  * Does nothing, you cant reload the item
+  */
+  public void reload(){
+
+  }
+
+
+}
