2cfe65998a3a8f843828bce0f01ad09385f9a107
root <root@localhost.localdomain>
2019-01-06 22:10:57 -0500

    fix transmision of enities

--- corrosion/network/Server.java
+++ corrosion/network/Server.java
@@ -206,7 +206,7 @@ public class Server{
   */
   public void newClient(Connection c){
     Protocol.send(4, c.id, c);
-
+    //TODO sync???
     for (int iEntity = 0; i < activeEntities.size(); ++iEntity){
       Protocol.send(9,activeEntities.get(iEntity),c);
     }
