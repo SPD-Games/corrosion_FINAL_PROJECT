817811da977bea0a345ab38a5a48eec84dd4013f
Henry Lim <45436409+henrylimb69@users.noreply.github.com>
2019-01-08 06:45:57 +1200

    Update inventoryBox.java

--- corrosion/inventoryBox.java
+++ corrosion/inventoryBox.java
@@ -1,1 +1,38 @@
+//Henry Lim
+//Jan. 8th, 2019
+//Inventory Box
+package inventorybox;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class InventoryBox extends JFrame{
+
+    public InventoryBox(){
+        initUI();
+    }
+    
+    private void initUI() {        
+        setTitle("Inventory");
+        add(new DrawingSurface());
+        setSize(340, 360);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                InventoryBox windowFrame = new InventoryBox();
+                windowFrame.setVisible(true);
+            }
+        });
+    }
+    
+}
 
