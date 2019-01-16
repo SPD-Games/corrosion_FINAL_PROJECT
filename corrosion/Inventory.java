ca8aad904721dfb65843cdf50faab6d6dd4cbda0
michael <michaelmetzinger1@gmail.com>
2019-01-15 20:17:20 -0500

    changed spawn items

--- corrosion/Inventory.java
+++ corrosion/Inventory.java
@@ -29,7 +29,7 @@ public class Inventory{
   *Constructor
   */
   public Inventory(){
-
+    /*
     hotBar[0] = new CrossBow();
     hotBar[1] = new Pistol();
     hotBar[3] = new Rifle();
@@ -48,11 +48,11 @@ public class Inventory{
     items[2][0] = new Apple(10);
     items[2][1] = new Bandage(10);
     items[2][2] = new Medkit(10);
+    */
 
-    /*
     hotBar[0] = new Tool();
     hotBar[1] = new BuildingPlan();
-    hotBar[2] = new UpgradePlan();*/
+    hotBar[2] = new UpgradePlan();
   }
 
   /**
